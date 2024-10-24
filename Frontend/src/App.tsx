1ec450696ded0f9dd748cf9170c6845c9d2253c8
Yoos99 <yoosh2520@naver.com>
2024-10-24 11:10:55 +0900

    remove: 프론트엔드 폴더 삭제

--- Frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <p>프로젝트</p>
-    </>
-  );
-}
-
-export default App;
--- Frontend/src/constants/index.ts
+++
@@ -1,8 +0,0 @@
-const greeting = "Hello World";
-console.log(greeting);
-
-var a = 3;
-
-const item = new Object();
-
-const a = item;
