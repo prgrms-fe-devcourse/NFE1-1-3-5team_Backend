60211b328fd33272093dfb3474e51e84d6c5bbe6
wilderif <woojuro3@gmail.com>
2024-10-31 14:44:21 +0900

    feat: index.ts 파일 app.ts 파일로 변경
    - nodemon.json app.ts 파일로 변경

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,21 @@
 import express, { Request, Response, Router } from "express";
 import cors from "cors";
+
 import auth from "./routes/auth";
 
 const app = express();
-const PORT = 3000;
-
-// CORS 설정
-app.use(cors());
+const PORT = process.env.PORT || 3000;
 
 // 미들웨어 설정
+// CORS 설정
+app.use(cors());
+// body-parser 설정
 app.use(express.json());
-
-// 기본 라우트
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Express + TypeScript Server is running?");
-});
 
 // 사용자 라우트 설정
 app.use("/api/auth", auth);
 
 // 서버 실행
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
