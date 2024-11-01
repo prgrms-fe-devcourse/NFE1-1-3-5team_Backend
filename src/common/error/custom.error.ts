1d0811f9bb676e4ff52f82f3589699cfd34dfa59
wilderif <woojuro3@gmail.com>
2024-11-01 14:21:49 +0900

    feat: Error 메세지 상태코드 관리를 위한 custom.error.ts 파일 작성

---
+++ src/common/error/custom.error.ts
@@ -0,0 +1,35 @@
+export class CustomError extends Error {
+  constructor(readonly message: string, readonly statusCode: number) {
+    super(message);
+  }
+}
+
+export class BadRequestError extends CustomError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class UnauthorizedError extends CustomError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
+
+export class ForbiddenError extends CustomError {
+  constructor(message: string) {
+    super(message, 403);
+  }
+}
+
+export class NotFoundError extends CustomError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+
+export class InternalServerError extends CustomError {
+  constructor(message: string = "Internal Server Error") {
+    super(message, 500);
+  }
+}
