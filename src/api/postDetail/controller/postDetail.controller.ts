edcd357406ca68a10933434eec5d256c33ca3676
wilderif <woojuro3@gmail.com>
2024-11-04 16:36:07 +0900

    feat: postDetail.service.ts 파일 작성
    - 파일 내부에 createNewPost, getPostById, updatePostById, deletePostById 함수 구현

--- src/api/postDetail/controller/postDetail.controller.ts
+++ src/api/postDetail/controller/postDetail.controller.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from "express";
 
-export const postPostDetail = async () => {};
+export const postPostDetail = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try 
+};
 
-export const getPostDetail = async () => {};
+export const getPostDetail = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
 
-export const patchPostDetail = async () => {};
+export const patchPostDetail = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
 
-export const deletePostDetail = async () => {};
+export const deletePostDetail = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
--- src/api/postDetail/service/postDetail.service.ts
+++ src/api/postDetail/service/postDetail.service.ts
@@ -1,7 +1,78 @@
-export const createPost = async () => {};
+import { Post } from "@prisma/client";
+import {
+  createPost,
+  findPostById,
+  updatePost,
+  deletePost,
+} from "../repository/postDetail.repository";
+import { PostCreateDto } from "../dto/PostCreate.dto";
+import { PostUpdateDto } from "../dto/PostUpdate.dto";
+import {
+  InternalServerError,
+  NotFoundError,
+} from "../../../common/error/custom.error";
 
-export const getPost = async () => {};
+export const createNewPost = async (
+  postCreateDto: PostCreateDto
+): Promise<Post> => {
+  try {
+    const newPost = await createPost(postCreateDto);
 
-export const updatePost = async () => {};
+    return newPost;
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      throw error;
+    }
 
-export const deletePost = async () => {};
+    console.error(`Error creating new post`, error);
+    throw new InternalServerError(`Error creating new post`);
+  }
+};
+
+export const getPostById = async (postId: string): Promise<Post> => {
+  try {
+    const post = await findPostById(postId);
+
+    return post;
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      throw error;
+    }
+
+    console.error(`Error fetching post by id: ${postId}`, error);
+    throw new InternalServerError(`Error fetching post by id: ${postId}`);
+  }
+};
+
+export const updatePostById = async (
+  postId: string,
+  postUpdateDto: PostUpdateDto
+): Promise<Post> => {
+  try {
+    const updatedPost = await updatePost(postId, postUpdateDto);
+
+    return updatedPost;
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      throw error;
+    }
+
+    console.error(`Error updating post by id: ${postId}`, error);
+    throw new InternalServerError(`Error updating post by id: ${postId}`);
+  }
+};
+
+export const deletePostById = async (postId: string): Promise<Post> => {
+  try {
+    const deletedPost = await deletePost(postId);
+
+    return deletedPost;
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      throw error;
+    }
+
+    console.error(`Error deleting post by id: ${postId}`, error);
+    throw new InternalServerError(`Error deleting post by id: ${postId}`);
+  }
+};
