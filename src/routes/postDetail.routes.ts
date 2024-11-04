9a6be8f2d3ad10cea2b94e298c88d80e897a0e28
wilderif <woojuro3@gmail.com>
2024-11-04 17:02:16 +0900

    feat: postDetail 컨트롤러 엔트포인트 postDetail.routes.ts 파일에 등록

--- src/routes/postDetail.routes.ts
+++ src/routes/postDetail.routes.ts
@@ -1,17 +1,23 @@
 import express from "express";
+import {
+  deletePostDetail,
+  getPostDetail,
+  patchPostDetail,
+  postPostDetail,
+} from "../api/postDetail/controller/postDetail.controller";
 
 const router = express.Router();
 
 // post detail 작성 라우트
-router.post("/", () => {});
+router.post("/", postPostDetail);
 
 // post detail 조회 라우트
-router.get("/:id", () => {});
+router.get("/:id", getPostDetail);
 
 // post detail 수정 라우트
-router.patch("/:id", () => {});
+router.patch("/:id", patchPostDetail);
 
 // post detail 삭제 라우트
-router.delete("/:id", () => {});
+router.delete("/:id", deletePostDetail);
 
 export default router;
